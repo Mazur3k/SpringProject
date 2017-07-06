cfef7904185a5403087c138bc4c3ba72b06fda2b
Adam Mazurkiewicz <adam.mazurkiewicz92@gmail.com>
2017-07-06 04:50:06 +0200

    Home.html changes

--- src/main/java/org/edu/mazurek/edu/EduApplication.java
+++ src/main/java/org/edu/mazurek/edu/EduApplication.java
@@ -1,10 +1,12 @@
 package org.edu.mazurek.edu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class EduApplication {
 
 	public static void main(String[] args) {
