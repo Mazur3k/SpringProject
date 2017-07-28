e7f45b49e98ec41c419920fa49dbbda204d45f69
Adam Mazurkiewicz <adam.mazurkiewicz92@gmail.com>
2017-07-28 23:01:25 +0200

    i18n 2/2

--- src/main/java/org/edu/mazurek/edu/controllers/HomeController.java
+++ src/main/java/org/edu/mazurek/edu/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package org.edu.mazurek.edu.controllers;
 
 import org.edu.mazurek.edu.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
