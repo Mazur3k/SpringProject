05354924322a4fc897a72d537cbbbb7aff66dd9b
Adam Mazurkiewicz <adam.mazurkiewicz92@gmail.com>
2017-08-01 03:33:57 +0200

    sign up modification

--- src/main/java/org/edu/mazurek/edu/model/User.java
+++ src/main/java/org/edu/mazurek/edu/model/User.java
@@ -1,11 +1,15 @@
 package org.edu.mazurek.edu.model;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Setter
@@ -20,9 +24,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty
+    @Size(min=2, max=30)
     private String firstname;
+    @NotEmpty
+    @Size(min=2, max=30)
     private String lastname;
+    @Email
     private String email;
+    @NotEmpty
+
     private String birthdate;
+    @NotNull
     private String password;
 }
